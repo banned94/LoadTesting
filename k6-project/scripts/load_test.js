e4b49947829be194661e58b33314d0a3745f83e5
Krishna <krishna.yudha.agastya@gmail.com>
2026-02-28 14:36:15 +0800

    refactor

--- k6-project/scripts/load_test.js
+++ k6-project/scripts/load_test.js
@@ -1,20 +1,28 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
+import { check, sleep } from 'k6';
 
 export let options = {
-  stages: [
-    { duration: '30s', target: 50 },
-    { duration: '1m', target: 50 },
-    { duration: '30s', target: 0 },
-  ],
+  vus: 10,        // virtual users
+  duration: '15s',   // test duration
   thresholds: {
-    http_req_duration: ['p(95)<500'],
+    http_req_duration: ['p(70)<500'], // 95% of requests should be below 500ms
   },
 };
 
 export default function () {
-  const url = __ENV.TARGET_URL || 'https://test.k6.io';
-  const res = http.get(url);
-  check(res, { 'status was 200': (r) => r.status === 200 });
+  let res = http.get (
+    'https://user:passwd@httpbin.org/basic-auth/user/passwd',
+    {
+  });
+
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'response time < 500ms': (r) => r.timings.duration < 500,
+    'authenticated': (r) => r.json().authenticated === true,
+    
+
+  });
+
   sleep(1);
 }
